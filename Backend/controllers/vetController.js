cf9ff825d6821957cf01a6144ee194607401e4ef
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-22 14:43:18 -0600

    fix: validate if email already exists in DB

--- Backend/controllers/vetController.js
+++ Backend/controllers/vetController.js
@@ -2,11 +2,16 @@ import Vet from '../models/Vet.js';
 
 const addVet = async (req, res) => {
     try {
-
+        // Avoid duplicate email
+        const { email } = req.body;
+        const vetExist = await Vet.findOne({ email });
+        if (vetExist) {
+            return res.status(400).json({error: `Email already exists`});
+        }
         const vet = new Vet(req.body);
         const vetSaved = await vet.save();
 
-        return res.json({message: `Vet added`, vet: vetSaved});
+        return res.status(200).json({message: `Vet added`, vet: vetSaved});
     } catch (error) {
         return res.status(500).json({error: `Internal server error: ${error.message}`});
     }
