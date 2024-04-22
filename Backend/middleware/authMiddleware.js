41760089c71f823b3ce5abbd870c40c675abb34e
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-22 16:39:46 -0600

    feat: validate and authentificate token with a middleware

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import Vet from '../models/Vet.js';
+
+// Validate the token sent by the user in the request headers
+const authMiddleware = async (req, res, next) => {
+    try {
+        let token = req.headers.authorization;
+        if(!token) return res.status(401).json({error: `Token not provided`});
+        if(!token.startsWith('Bearer')) return res.status(401).json({error: `Invalid token`});
+        token = token.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.vet = await Vet.findById({ _id: decoded.uid }).select('-password -token -confirmed');
+        return next();
+    } catch (error) {
+        return res.status(500).json({error: `Internal server error: ${error.message}`});
+    }
+}
+
+export default authMiddleware;
--- Backend/routes/vetRoutes.js
+++ Backend/routes/vetRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { addVet, confirmVet, authentificateVet } from '../controllers/vetController.js';
+import { addVet, confirmVet, authentificateVet, vetProfile } from '../controllers/vetController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', addVet);
-router.get('/confirm/:token', confirmVet)
-router.post('/login', authentificateVet)
+router.get('/confirm/:token', confirmVet);
+router.post('/login', authentificateVet);
+
+router.get('/profile', authMiddleware, vetProfile)
 
 export default router;
