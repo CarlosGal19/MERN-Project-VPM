8b6615ee731203d2e498d6a95fd57745af100544
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-22 13:15:31 -0600

    feat: config environment variables

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const db = await mongoose.connect('mongodb+srv://admin:root@cluster0.6jc7vwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+        const db = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import connectDB from './config/db.js';
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
 
 connectDB();
 
