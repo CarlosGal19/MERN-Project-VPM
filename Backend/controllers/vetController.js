6908885b41b8c007be6d2c299e7c00709e6cc028
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-22 17:06:27 -0600

    style: give format to code

--- Backend/controllers/vetController.js
+++ Backend/controllers/vetController.js
@@ -8,29 +8,29 @@ const addVet = async (req, res) => {
         const { email } = req.body;
         const vetExist = await Vet.findOne({ email });
         if (vetExist) {
-            return res.status(400).json({error: `Email already exists`});
+            return res.status(400).json({ error: `Email already exists` });
         }
         const vet = new Vet(req.body);
         const vetSaved = await vet.save();
 
-        return res.status(200).json({message: `Vet added`, vet: vetSaved});
+        return res.status(200).json({ message: `Vet added`, vet: vetSaved });
     } catch (error) {
-        return res.status(500).json({error: `Internal server error: ${error.message}`});
+        return res.status(500).json({ error: `Internal server error: ${error.message}` });
     }
 }
 
 const confirmVet = async (req, res) => {
     try {
         const { token } = req.params;
-        if(!token) return res.status(400).json({error: `Token not provided`});
+        if (!token) return res.status(400).json({ error: `Token not provided` });
         const vetConfirm = await Vet.findOne({ token });
-        if(!vetConfirm) return res.status(404).json({error: `Vet not found`});
+        if (!vetConfirm) return res.status(404).json({ error: `Vet not found` });
         vetConfirm.token = null;
         vetConfirm.confirmed = true;
         await vetConfirm.save();
-        return res.status(200).json({message: `Vet confirmed`});
+        return res.status(200).json({ message: `Vet confirmed` });
     } catch (error) {
-        return res.status(500).json({error: `Internal server error: ${error.message}`});
+        return res.status(500).json({ error: `Internal server error: ${error.message}` });
     }
 }
 
@@ -38,67 +38,67 @@ const authentificateVet = async (req, res) => {
     try {
         const { email, password } = req.body;
         const vet = await Vet.findOne({ email });
-        if(!vet) return res.status(404).json({error: `Vet not found`});
-        if(!vet.confirmed) return res.status(403).json({error: `Vet not confirmed`});
+        if (!vet) return res.status(404).json({ error: `Vet not found` });
+        if (!vet.confirmed) return res.status(403).json({ error: `Vet not confirmed` });
         if (!await vet.authenticate(password)) {
-            return res.status(401).json({error: `Invalid password`});
+            return res.status(401).json({ error: `Invalid password` });
         }
-        return res.status(200).json({message: `Vet logged`, token: generateJWT(vet._id, vet.name)});
+        return res.status(200).json({ message: `Vet logged`, token: generateJWT(vet._id, vet.name) });
     } catch (error) {
-        return res.status(500).json({error: `Internal server error: ${error.message}`});
+        return res.status(500).json({ error: `Internal server error: ${error.message}` });
     }
 }
 
 const vetProfile = async (req, res) => {
     try {
         const { vet } = req;
-        if(!vet) return res.status(404).json({error: `Vet not found`});
-        return res.status(200).json({message: `Vet profile`, vet: vet});
+        if (!vet) return res.status(404).json({ error: `Vet not found` });
+        return res.status(200).json({ message: `Vet profile`, vet: vet });
     } catch (error) {
-        return res.status(500).json({error: `Internal server error: ${error.message}`});
+        return res.status(500).json({ error: `Internal server error: ${error.message}` });
     }
 }
 
 const resetPassword = async (req, res) => {
     try {
         const { email } = req.body;
-        if (!email) return res.status(400).json({error: `Email not provided`});
+        if (!email) return res.status(400).json({ error: `Email not provided` });
         const vet = await Vet.findOne({ email });
-        if(!vet) return res.status(404).json({error: `Vet not found`});
+        if (!vet) return res.status(404).json({ error: `Vet not found` });
         vet.token = generateID();
         await vet.save();
-        return res.status(200).json({message: `Token with instructions sent`, token: vet.token});
+        return res.status(200).json({ message: `Token with instructions sent`, token: vet.token });
     } catch (error) {
-        return res.status(500).json({error: `Internal server error: ${error.message}`});
+        return res.status(500).json({ error: `Internal server error: ${error.message}` });
     }
 }
 
 const validateToken = async (req, res) => {
     try {
         const { token } = req.params;
-        if(!token) return res.status(400).json({error: `Token not provided`});
+        if (!token) return res.status(400).json({ error: `Token not provided` });
         const vetToken = await Vet.findOne({ token });
-        if(!vetToken) return res.status(404).json({error: `Vet not found`});
-        return res.status(200).json({message: `Token validated`});
+        if (!vetToken) return res.status(404).json({ error: `Vet not found` });
+        return res.status(200).json({ message: `Token validated` });
     } catch (error) {
-        return res.status(500).json({error: `Internal server error: ${error.message}`});
+        return res.status(500).json({ error: `Internal server error: ${error.message}` });
     }
 }
 
 const newPassword = async (req, res) => {
     try {
         const { token } = req.params;
-        if(!token) return res.status(400).json({error: `Token not provided`});
+        if (!token) return res.status(400).json({ error: `Token not provided` });
         const vetToken = await Vet.findOne({ token });
-        if(!vetToken) return res.status(404).json({error: `Vet not found`});
+        if (!vetToken) return res.status(404).json({ error: `Vet not found` });
         const { password } = req.body;
-        if(!password) return res.status(400).json({error: `Password not provided`});
+        if (!password) return res.status(400).json({ error: `Password not provided` });
         vetToken.password = password;
         vetToken.token = null;
         await vetToken.save();
-        return res.status(200).json({message: `Password updated`});
+        return res.status(200).json({ message: `Password updated` });
     } catch (error) {
-        return res.status(500).json({error: `Internal server error: ${error.message}`});
+        return res.status(500).json({ error: `Internal server error: ${error.message}` });
     }
 }
 
