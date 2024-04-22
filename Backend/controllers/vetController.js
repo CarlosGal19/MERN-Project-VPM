a399e63f15d2733b514aee89a867501e9854af28
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-22 16:59:46 -0600

    feat: validate that token provided will be correct

--- Backend/controllers/vetController.js
+++ Backend/controllers/vetController.js
@@ -73,9 +73,13 @@ const resetPassword = async (req, res) => {
     }
 }
 
-const validateToken = async () => {
+const validateToken = async (req, res) => {
     try {
-
+        const { token } = req.params;
+        if(!token) return res.status(400).json({error: `Token not provided`});
+        const vetToken = await Vet.findOne({ token });
+        if(!vetToken) return res.status(404).json({error: `Vet not found`});
+        return res.status(200).json({message: `Token validated`});
     } catch (error) {
         return res.status(500).json({error: `Internal server error: ${error.message}`});
     }
