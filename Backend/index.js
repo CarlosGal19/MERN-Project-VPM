a1207f0a583b6cc2f6cf12c15ca011ff55212504
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-22 13:05:44 -0600

    feat: create express app

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,12 @@
-console.log('from node js')
+import express from 'express';
+import connectDB from './config/db';
+
+const app = express()
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000')
+});
+
+app.use('/', (req, res) => {
+    res.send('Hello World!');
+});
