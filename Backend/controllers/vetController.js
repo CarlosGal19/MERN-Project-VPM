212a16a8979a990ce1c91070091087f34ffc9629
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-22 15:15:25 -0600

    feat: confirm vet by token

--- Backend/controllers/vetController.js
+++ Backend/controllers/vetController.js
@@ -17,6 +17,22 @@ const addVet = async (req, res) => {
     }
 }
 
+const confirmVet = async (req, res) => {
+    try {
+        const { token } = req.params;
+        if(!token) return res.status(400).json({error: `Token not provided`});
+        const vetConfirm = await Vet.findOne({ token });
+        if(!vetConfirm) return res.status(404).json({error: `Vet not found`});
+        vetConfirm.token = null;
+        vetConfirm.confirmed = true;
+        await vetConfirm.save();
+        return res.status(200).json({message: `Vet confirmed`});
+    } catch (error) {
+        return res.status(500).json({error: `Internal server error: ${error.message}`});
+    }
+}
+
 export {
-    addVet
+    addVet,
+    confirmVet
 }
--- Backend/routes/vetRoutes.js
+++ Backend/routes/vetRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { addVet } from '../controllers/vetController.js';
+import { addVet, confirmVet } from '../controllers/vetController.js';
 
 const router = express.Router();
 
 router.post('/', addVet);
+router.get('/confirm/:token', confirmVet)
 
 export default router;
