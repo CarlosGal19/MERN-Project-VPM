602319254f4f35e6be7a15d7e816a5cce060edc8
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-22 14:42:36 -0600

    fix: remove getProfile function

--- Backend/routes/vetRoutes.js
+++ Backend/routes/vetRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getProfile, addVet } from '../controllers/vetController.js';
+import { addVet } from '../controllers/vetController.js';
 
 const router = express.Router();
 
