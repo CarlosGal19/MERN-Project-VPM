27bd01ca192e199b31ac2b0c13fd749bf664f182
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-23 12:49:06 -0600

    feat: validate that vet is logged for adding a patient

--- Backend/controllers/patientController.js
+++ Backend/controllers/patientController.js
@@ -1,10 +1,11 @@
 import Patient from '../models/Patient.js';
 
-const addPatient = (req, res) => {
+const addPatient = async (req, res) => {
     try {
         const patient =  new Patient(req.body);
-        console.log(patient);
-        return res.status(200).json({ message: 'POST /patients' });
+        patient.vet_id = req.vet._id;
+        await patient.save();
+        return res.status(200).json({ message: 'Patient added', patient: patient});
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
--- Backend/routes/patientRoutes.js
+++ Backend/routes/patientRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getPatients, addPatient } from '../controllers/patientController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').get(getPatients).post(addPatient)
+router.route('/').get(getPatients).post(authMiddleware, addPatient)
 
 
 export default router;
