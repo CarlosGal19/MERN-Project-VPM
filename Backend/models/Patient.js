fe9013f18be458680dc48f6e52737f4a11c28993
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-23 12:07:34 -0600

    feat: create patient model

---
+++ Backend/models/Patient.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const patientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    symptoms: {
+        type: String,
+        required: true
+    },
+    vet_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Vet',
+        required: true
+    }
+});
+
+const Patient = mongoose.model('Patient', patientSchema);
+
+export default Patient;
