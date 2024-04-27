b86ed7fb34419606042d379ebd3d40fe26a44c39
Carlos Galindo <utm22030587@utma.edu.mx>
2024-04-27 14:32:13 -0600

    feat: add credentials of nodemailer to send confirmation email

---
+++ Backend/helpers/registerEmail.js
@@ -0,0 +1,14 @@
+import nodeMailer from 'nodemailer';
+
+const registerEmail = (email, name) => {
+    var transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS
+        }
+      });
+}
+
+export default registerEmail;
