4ff4aeda033e6b92b1b84522e5e2634e0f7c3ec8
Carlos Galindo <utm22030587@utma.edu.mx>
2024-04-28 12:22:22 -0600

    test: remove debbug helpers

--- frontend/src/components/Alert.jsx
+++ frontend/src/components/Alert.jsx
@@ -1,5 +1,4 @@
 const Alert = (alert) => {
-    console.log(alert)
   return (
     <>
         <div className= { `${alert.type === 'alert' ? 'bg-red-600' : 'bg-indigo-600'} alert alert-danger text-center block text-white text-bold text-xl rounded-2xl py-5`} role="alert">
