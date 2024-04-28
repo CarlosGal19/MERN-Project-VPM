765bb8b3e5816f3c5c4c395144ae8dd1a7fa76db
Carlos Galindo <utm22030587@utma.edu.mx>
2024-04-27 18:54:58 -0600

    feat: identify user for its email to recover password

--- frontend/src/pages/ForgetPassword.jsx
+++ frontend/src/pages/ForgetPassword.jsx
@@ -1,16 +1,47 @@
 import { Link }  from 'react-router-dom'
+import { useState } from 'react'
+import Alerta from '../components/Alert'
+import axiosClient from '../config/axios'
 
 const ForgetPassword = () => {
+
+    const [email, setEmail] = useState('');
+    const [alert, setAlert] = useState(null);
+
+    const handleSubmit = async (e) => {
+        try {
+          e.preventDefault();
+          if (!email || email.length < 10) {
+            setAlert({
+              type: 'alert',
+              msg: 'Email is required'
+            });
+            return;
+          }
+          setAlert(null);
+          const response = await axiosClient.post('/reset-password', { email });
+          console.log(response);
+        } catch (error) {
+          setAlert({
+            type: 'alert',
+            msg: error.response.data.error
+          });
+        }
+    }
+
     return (
         <>
           <div>
             <h1 className="text-indigo-600 font-black text-6xl">Recover your Access and not Stray your <span className="text-black">Patients</span></h1>
           </div>
           <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-            <form>
+            {
+              alert && <Alerta type={alert.type} msg={alert.msg} />
+            }
+            <form onSubmit={handleSubmit}>
               <div className="my-6">
                 <label htmlFor="email" className="uppercase text-gray-600 block text-xl font-bold">Email</label>
-                <input type="email" id="email" placeholder="Your email" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"/>
+                <input value={email} onChange={e => setEmail(e.target.value)} type="email" id="email" placeholder="Your email" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"/>
               </div>
               <input type="submit" value="Send instructions" className="bg-indigo-700 text-white font-bold rounded-xl w-full py-3 uppercase mt-5  hover:cursor-pointer hover:bg-indigo-900 md:w-auto px-16" />
             </form>
