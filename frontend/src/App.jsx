5b813fddbb91c0e4b82a0255414d44fcacb16121
Carlos Galindo <utm22030587@utma.edu.mx>
2024-05-18 17:05:04 -0600

    feat: involve app in context provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Register from './pages/Register'
 import ForgetPassword from './pages/ForgetPassword'
 import ConfirmAccount from './pages/ConfirmAccount'
 import NewPassword from './pages/NewPassword'
+import { AuthProvider } from './context/AuthProvider'
 import './index.css'
 
 function App() {
@@ -12,15 +13,17 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<AuthLayout/>}>
-            <Route index element={<Login/>} />
-            <Route path='register' element={<Register/>} />
-            <Route path='forget-password' element={<ForgetPassword/>} />
-            <Route path='forget-password/:token' element={<NewPassword/>} />
-            <Route path='confirm-account/:id' element={<ConfirmAccount/>} />
-          </Route>
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path='/' element={<AuthLayout/>}>
+              <Route index element={<Login/>} />
+              <Route path='register' element={<Register/>} />
+              <Route path='forget-password' element={<ForgetPassword/>} />
+              <Route path='forget-password/:token' element={<NewPassword/>} />
+              <Route path='confirm-account/:id' element={<ConfirmAccount/>} />
+            </Route>
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </>
   )
